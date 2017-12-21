aba6701990d09efd098777293d334754588061a9
Tiger Zhang <littlebiao@outlook.com>
2017-12-21 11:17:52 +0000

    tiger

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150809022253) do
+ActiveRecord::Schema.define(version: 20171221075337) do
 
-  create_table "posters", force: :cascade do |t|
-    t.text   "content"
-    t.string   "label"
-    t.datetime "release_date"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+  create_table "articles", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.string   "label",      limit: 255
+    t.text     "text",       limit: 65535
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.string   "commenter",  limit: 255
+    t.text     "body",       limit: 65535
+    t.integer  "article_id", limit: 4
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id", using: :btree
+
+  add_foreign_key "comments", "articles"
 end
