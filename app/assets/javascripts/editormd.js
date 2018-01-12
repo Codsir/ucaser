bf0a262d15633986a7747241e516a18dc2a54a0d
wang peng <wangpeng246300@163.com>
2018-01-12 08:40:27 +0000

    Peng's 3rd UI

--- app/assets/javascripts/editormd.js
+++ app/assets/javascripts/editormd.js
@@ -3365,7 +3365,7 @@
 
     // Emoji graphics files url path
     editormd.emoji     = {
-        path  : "../../plugins/emoji-dialog/emoji/",
+        path  : "/plugins/emoji-dialog/emoji/",
         ext   : ".png"
     };
 
